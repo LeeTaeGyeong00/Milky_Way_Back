9620940e0f6efcf2258506f7cff63e3085df9aec
bibi <youbinking@naver.com>
2024-03-24 04:16:19 +0900

    Setting

---
+++ src/main/java/com/example/milky_way_back/MilkyWayBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.milky_way_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkyWayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkyWayBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/milky_way_back/MilkyWayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milky_way_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyWayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
